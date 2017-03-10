5600433aef9e9d8ef5fae77fdfa40c6343dd9f68
Felix Arntz <felix-arntz@leaves-and-love.net>
2017-03-10 18:11:17 +0100

    Initial commit.

---
+++ src/structure-wordpress.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Structure loader.
+ *
+ * @package APIAPIStructureWordPress
+ * @since 1.0.0
+ */
+
+if ( ! function_exists( 'apiapi_register_structure_wordpress' ) ) {
+
+	/**
+	 * Registers the structure for the API of any WordPress site.
+	 *
+	 * It is stored in a global if the API-API has not yet been loaded.
+	 *
+	 * @since 1.0.0
+	 */
+	function apiapi_register_structure_wordpress( $name, $base_uri ) {
+		$structure = new APIAPI\Structure_WordPress\Structure_WordPress( $name, $base_uri );
+
+		if ( function_exists( 'apiapi_manager' ) ) {
+			apiapi_manager()->structures()->register( $name, $structure );
+		} else {
+			if ( ! isset( $GLOBALS['_apiapi_structures_loader'] ) ) {
+				$GLOBALS['_apiapi_structures_loader'] = array();
+			}
+
+			$GLOBALS['_apiapi_structures_loader'][ $name ] = $structure;
+		}
+	}
+
+	apiapi_register_structure_wordpress( 'wordpress-com', 'https://public-api.wordpress.com/wp/v2/' );
+	apiapi_register_structure_wordpress( 'wordpress-org', 'https://wordpress.org/wp-json/' );
+
+}
